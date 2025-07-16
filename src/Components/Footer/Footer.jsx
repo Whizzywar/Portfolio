23dede489d37d9190b587f95d7eea286e8e19cec
Whizzyware <ubaniwisdom480@gmail.com>
2025-07-16 04:41:20 +0100

    enhanced

--- src/Components/Footer/Footer.jsx
+++ src/Components/Footer/Footer.jsx
@@ -1,6 +1,13 @@
 import React from "react";
 import "./Footer.css";
 import user_icon from "../../assets/user_icon.svg";
+import {
+  FaGithub,
+  FaLinkedin,
+  FaTwitter,
+  FaWhatsapp,
+  FaInstagram,
+} from "react-icons/fa";
 
 const Footer = () => {
   return (
@@ -12,6 +19,43 @@ const Footer = () => {
             I am a frontend developer from nigeria, with 4 years experience in
             companies like Microsoft, Tesla and Apple
           </p>
+          <div className="social-icons">
+            <a
+              href="https://github.com"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              <FaGithub className="icon" />
+            </a>
+            <a
+              href="https://linkedin.com"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              <FaLinkedin className="icon" />
+            </a>
+            <a
+              href="https://twitter.com"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              <FaTwitter className="icon" />
+            </a>
+            <a
+              href={`https://wa.me/+2348140067333`}
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              <FaWhatsapp className="whatsapp-icon" />
+            </a>
+            <a
+              href="https://instagram.com"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              <FaInstagram className="icon" />
+            </a>
+          </div>
         </div>
         <div className="footer-top-right">
           <div className="footer-email-input">
@@ -23,12 +67,14 @@ const Footer = () => {
       </div>
       <hr />
       <div className="footer-bottom">
-        <p className="footer-bottom">© 2025 Whizzyware. All rights reserved.</p>
         <div className="footer-bottom-right">
           <p>Term of Services</p>
           <p>Privacy Policy</p>
           <p>Connect with me</p>
         </div>
+        <p className="footer-bottom-left">
+          Developed by © Whizzyware 2025. | All rights reserved.
+        </p>
       </div>
     </div>
   );
